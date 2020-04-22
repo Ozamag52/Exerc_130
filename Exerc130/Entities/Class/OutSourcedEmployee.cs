1b4e0ffbb6641ab1564e7056b5ed2e2c291a68ea
henriquegustavo2010@gmail.com <henriquegustavo2010@gmail.com>
2020-04-22 14:01:45 -0300

    Versao final

---
+++ Exerc130/Entities/Class/OutSourcedEmployee.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Exerc130.Entities.Class
+{
+    class OutSourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+    
+        public OutSourcedEmployee()
+        {
+
+        }
+        public OutSourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge): base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public sealed override double Payment()
+        {
+            return (base.Payment()) + (double)(1.1 * AdditionalCharge);
+        }
+
+
+    }
+}
--- Exerc130/Program.cs
+++ Exerc130/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
-
+using Exerc130.Entities.Class;
+using System.Collections.Generic;
 namespace Exerc130
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Employee> employeeList = new List<Employee>();
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+            
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Employee #" + i + " data: ");
+                Console.Write("Outsourced (y/n)? ");
+                char employeeType = char.Parse(Console.ReadLine());
+                if (employeeType == 'n' || employeeType == 'N')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Hours: ");
+                    int hours = int.Parse(Console.ReadLine());
+                    Console.Write("Value per hour: ");
+                    double valuePerHour = double.Parse(Console.ReadLine());
+                    Employee employee = new Employee(name, hours, valuePerHour);
+                    employeeList.Add(employee);
+                }
+                else if (employeeType == 'y' || employeeType == 'Y')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Hours: ");
+                    int hours = int.Parse(Console.ReadLine());
+                    Console.Write("Value per hour: ");
+                    double valuePerHour = double.Parse(Console.ReadLine());
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine());
+                    Employee employee = new OutSourcedEmployee(name, hours, valuePerHour,additionalCharge);
+                    employeeList.Add(employee);
+                }
+                
+            }
+            Console.WriteLine("PAYMENT");
+            foreach (Employee e in employeeList)
+            {
+                Console.WriteLine(e.Name + " - " + " $ " + e.Payment());
+            }
         }
     }
 }
